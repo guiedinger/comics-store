43718b0788154b4c4d6a9011e801703f99a8f313
Guilherme Edinger <guilherme@edinger.com.br>
2020-02-29 16:18:57 +0000

    Adjusted de key when loading

--- src/pages/ComicsPage.jsx
+++ src/pages/ComicsPage.jsx
@@ -19,9 +19,9 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default (props) => {
+export default () => {
   const classes = useStyles();
-  const [isLoading, setIsLoading ] = useState(false);
+  const [isLoading, setIsLoading ] = useState(true);
   const [comics, setComics] = useState(new Array(20).fill({}));
   const [totalComics, setTotalComics] = useState(0);
   const { page } = useParams();
@@ -44,15 +44,14 @@ export default (props) => {
         setComics(newComics);
         setTotalComics(res.data.data.total);
         setIsLoading(false);
-        console.log(newComics);
       });
   }, [currentPage, currentDate]);
 
   return (
     <div className={classes.root}>
       <div className={classes.comicsContainer}>
-        {comics && comics.map(comic =>
-          <ComicCard key={comic.id} {...comic} isLoading={isLoading} />)}
+        {comics && comics.map((comic, index) =>
+          <ComicCard key={isLoading ? index : comic.id} {...comic} isLoading={isLoading} />)}
       </div>
       <Paginator
         currentPage={currentPage}
