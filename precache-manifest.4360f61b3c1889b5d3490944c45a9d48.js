7d84ac7b55fd42e61da74696f5688cf72d752115
Guilherme Edinger <guilherme@edinger.com.br>
2020-08-05 13:21:25 +0000

    Updates

---
+++ precache-manifest.4360f61b3c1889b5d3490944c45a9d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e2beef6df0c13add855e0686c7c050",
+    "url": "/comics-store/index.html"
+  },
+  {
+    "revision": "cf9e34729fb772a43728",
+    "url": "/comics-store/static/css/main.deb39b56.chunk.css"
+  },
+  {
+    "revision": "733a3037cc8ccdbb242a",
+    "url": "/comics-store/static/js/2.5110e846.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/comics-store/static/js/2.5110e846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9e34729fb772a43728",
+    "url": "/comics-store/static/js/main.5b28f0b3.chunk.js"
+  },
+  {
+    "revision": "86c4f97f877124f193e8",
+    "url": "/comics-store/static/js/runtime-main.71b5c1d8.js"
+  }
+]);
